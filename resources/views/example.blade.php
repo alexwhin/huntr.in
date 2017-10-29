221c99d5535b3d4cc65621f2a8e6ed9ab73e6ce1
alexwhin <alexwhin@gmail.com>
2017-10-29 14:57:32 +0000

    Untracked

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -5,10 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Example</title>
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                text-align: center
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                padding: 60px 0 30px
+                margin: 0;
+            }
+        </style>
     </head>
     <body>
-
-        <!-- <iframe src="https://flowxo.com/" frameborder="0" style="width: 100%; height: 100%;"></iframe> -->
 
         <script type="text/javascript">
           window.huntr = {
